908725f680a2dcc67360b30a53d3acbdf00a8ee3
deivish <99086810+deivish@users.noreply.github.com>
2022-03-19 11:47:54 -0500

    Update PretestApplicationTests.java

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -1,11 +1,21 @@
 package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.Validate;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PretestApplicationTests {
 
     @Test
-    void contextLoads() {
+    void idPasswordEquals() {
+        String dbPasword = "Hola";
+        String Pasword = "Hola";
+
+        boolean result = UserUtils.isPasswordEqual(dbPasword, Pasword);
+
+        assertTrue(result);
+
     }
 
 }
